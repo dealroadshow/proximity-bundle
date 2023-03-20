6eb0c49d7c2a185ab8895e52c2f0d9d79a67f9b8
petr-buchyn <petr.buchyn@finsight.com>
2023-03-20 05:03:04 +0200

    Interceptors pass, configurable proxies namespace and dir

--- src/DealroadshowProximityBundle.php
+++ src/DealroadshowProximityBundle.php
@@ -2,22 +2,30 @@
 
 namespace Dealroadshow\Bundle\ProximityBundle;
 
+use Dealroadshow\Bundle\ProximityBundle\DependencyInjection\Compiler\InterceptorsPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class DealroadshowProximityBundle extends AbstractBundle
 {
-    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
-    {
-        $container->import('../config/dealroadshow_proximity.yaml');
-    }
-
     public function boot(): void
     {
         $proxiesDir = $this->container->getParameter('dealroadshow_proximity.proxy_classes_dir');
         if (!file_exists($proxiesDir)) {
             mkdir($proxiesDir, 0777, true);
         }
+    }
+
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new InterceptorsPass());
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/dealroadshow_proximity.yaml');
     }
 }
---
+++ src/DependencyInjection/Compiler/InterceptorsPass.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Dealroadshow\Bundle\ProximityBundle\DependencyInjection\Compiler;
+
+use Dealroadshow\Proximity\MethodsInterception\BodyInterceptorInterface;
+use Dealroadshow\Proximity\MethodsInterception\ResultInterceptorInterface;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class InterceptorsPass implements CompilerPassInterface
+{
+    public const BODY_INTERCEPTOR_TAG = 'dealroadshow_proximity.interceptor.body';
+    public const RESULT_INTERCEPTOR_TAG = 'dealroadshow_proximity.interceptor.result';
+
+
+    public function process(ContainerBuilder $container)
+    {
+        $container
+            ->registerForAutoconfiguration(BodyInterceptorInterface::class)
+            ->addTag(self::BODY_INTERCEPTOR_TAG);
+
+        $container
+            ->registerForAutoconfiguration(ResultInterceptorInterface::class)
+            ->addTag(self::RESULT_INTERCEPTOR_TAG);
+    }
+}
